3822a2a645c6732fb2eee92c15ea84145d319097
selenahans <selenahans49@gmail.com>
2026-01-29 11:36:33 +0700

    footer + preloader

--- portofolio/src/App.tsx
+++ portofolio/src/App.tsx
@@ -4,6 +4,7 @@
 // import './App.css'
 import DataImage from "./data";
 import { listTools } from "./data";
+import { listProyek } from "./data";
 function App() {
   // const [count, setCount] = useState(0)
 
@@ -17,6 +18,7 @@ function App() {
               src={DataImage.HeroImage}
               alt="Hero"
               className="w-10 rounded-md"
+              loading="lazy"
             />
             <q>lorem ipsum im so happy to see you!</q>
           </div>
@@ -43,6 +45,7 @@ function App() {
           src={DataImage.HeroImage}
           alt="Hero"
           className="w-[500px] md:ml-auto"
+          loading="lazy"
         />
       </div>
       <div className="tentang mt-32 py-10">
@@ -50,6 +53,7 @@ function App() {
           <img
             src={DataImage.HeroImage}
             alt="Image"
+            loading="lazy"
             className="w-12 rounded-md mb-10 sm:hidden"
           />
           <p className="text-base/loose mb-10">LALALLALALALA</p>
@@ -57,6 +61,7 @@ function App() {
             <img
               src={DataImage.HeroImage}
               alt="image"
+              loading="lazy"
               className="w-12 rounded-md sm:block hidden"
             />
             <div className="flex items-center gap-6">
@@ -80,25 +85,110 @@ function App() {
 
         <div className="tools mt-32 ">
           <h1 className="text-4xl/snug font-bold mb-4"> tools</h1>
-          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">tools yg aku pakai</p>
+          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
+            tools yg aku pakai
+          </p>
           <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-            {
-              listTools.map(tool => (
-            <div className="flex items-center  gap-2 p-3 border border-zinc-800 rounded-md hover:bg-zinc-200 group " key={tool.id}>
-              <img
-                src={tool.gambar}
-                alt="tools image"
-                className="w-14 bg-zinc-800 p-1 group-hover: bg-zinc-900"
-              />
-              <div>
-                <h4 className="font-bold">{tool.nama}</h4>
-                <p className="opacity-50">{tool.ket}</p>
+            {listTools.map((tool) => (
+              <div
+                className="flex items-center  gap-2 p-3 border border-zinc-800 rounded-md hover:bg-zinc-200 group "
+                key={tool.id}
+              >
+                <img
+                  src={tool.gambar}
+                  alt="tools image"
+                  className="w-14 bg-zinc-800 p-1 group-hover: bg-zinc-900"
+                />
+                <div>
+                  <h4 className="font-bold">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.ket}</p>
+                </div>
               </div>
-            </div>
-              ))
-            }
+            ))}
           </div>
         </div>
+      </div>
+
+      <div className="proyek mt-32 py-10">
+        <h1 className="text-center text-4xl font-bold mb-2">project</h1>
+        <p className="text-base/loose text-center opacity-50 ">
+          list project yang sudah aku buat
+        </p>
+        <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 ">
+          {listProyek.map((proyek) => (
+            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+              <img src={proyek.gambar} alt={proyek.nama} loading="lazy" />
+              <div>
+                <h1 className="text2xl font-bold my-4">{proyek.nama}</h1>
+                <p className="text-base/loose mb-4">{proyek.desk}</p>
+                <div className="flex flex-wrap gap-2">
+                  {proyek.tools.map((tool, index) => (
+                    <p
+                      className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold"
+                      key={index}
+                    >
+                      {tool}
+                    </p>
+                  ))}
+                </div>
+                <div className="mt-8 text-center">
+                  <a
+                    href="#"
+                    className="bg-violet-500 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-700"
+                  >
+                    Lihat detail
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* <div className="kontak mt-32 p-10">
+        <h1 className="text-4xl mb-2 font-bold text-center">kontak</h1>
+        <p className="text-base/loose text-center mb-10 opacity-50">
+          let's connect
+        </p>
+        <form action="">
+          <div>
+            <div>
+              <label>nama lengkap</label>
+              <input
+                type="text"
+                name="nama"
+                placeholder="masukkan nama"
+                required
+              />
+            </div>
+            <div>
+              <label>email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="masukkan email"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="pesan">pesan</label>
+              <textarea
+                name="pesan"
+                id="pesan"
+                cols="30"
+                rows="10"
+                placeholder="masukkan pesan"
+              ></textarea>
+            </div>
+            <div>
+              <button type="submit">Kirim</button>
+            </div>
+          </div>
+        </form>
+      </div> */}
+
+      <div>
+        
       </div>
     </>
   );
--- portofolio/src/main.tsx
+++ portofolio/src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+
 import Navbar from "./components/Navbar.tsx";
 import 'remixicon/fonts/remixicon.css';
+import Footer from "./components/Footer.tsx";
+import PreLoader from "./components/PreLoader.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <PreLoader />
     <div className="container mx-auto px-8">
       <Navbar />
       <App />
+      <Footer />
     </div>
   </StrictMode>,
 );
