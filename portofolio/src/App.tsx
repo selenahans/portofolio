f109328eca2f9383582275acf3580f2bf7770e6b
selenahans <selenahans49@gmail.com>
2026-01-29 17:12:34 +0700

    button, desc,

--- portofolio/src/App.tsx
+++ portofolio/src/App.tsx
@@ -13,7 +13,8 @@ function App() {
       {/* <h1 className="text-3xl text-red-600 text-shadow-black">hao</h1> */}
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div className="animate__animated animate__fadeInUp animate__delay-3s">
-          <div className="flex items-center gap-3 mb-6 bg-amber-300 w-fit p-4 rounded-2xl">
+          <h1 className="text-5xl/tight font-bold mb-6">Hi, I'm Selena Hans</h1>
+          {/* <div className="flex items-center gap-3 mb-6 bg-amber-300 w-fit p-4 rounded-2xl">
             <img
               src={DataImage.HeroImage}
               alt="Hero"
@@ -21,21 +22,29 @@ function App() {
               loading="lazy"
             />
             <q>lorem ipsum im so happy to see you!</q>
-          </div>
-          <h1 className="text-5xl/tight font-bold mb-6">
-            Hi, my name is Selena
-          </h1>
-          <p className="text-base/loose mb-6 opacity-50">lalalalla</p>
+          </div> */}
+          <p className="text-base/loose mb-6 font-semibold">
+            ● Based in Surabaya, Indonesia
+          </p>
+          <p className="text-base/loose mb-6 opacity-50">
+            As a third-year Informatics Engineering student at the University of
+            Surabaya, I have a deep interest in Software Engineering, UI/UX
+            Design, and Graphic Design. My main focus is on developing
+            efficient, visually appealing, and user-friendly interfaces and
+            systems. With hands-on experience in UI/UX and web development, I am
+            committed to continuous learning, exploring new technologies, and
+            adapting quickly to challenges in the tech industry.
+          </p>
           <div className="flex items-center sm:gap-4 gap-2">
             <a
               href="#"
-              className="bg-amber-200 p-4 rounded-2xl hover:bg-amber-400"
+              className="bg-[#E5DFD3] p-4 rounded-2xl hover:bg-[#A39383] hover:scale-110 transition-transform duration-300 text-[#232323]"
             >
               Download CV <i className="ri-download-line ri-lg "></i>
             </a>
             <a
               href="#"
-              className="bg-amber-200 p-4 rounded-2xl hover:bg-amber-400"
+              className="bg-[#E5DFD3] p-4 rounded-2xl hover:bg-[#A39383] hover:scale-110 transition-transform duration-300 text-[#232323]"
             >
               Lihat Project <i className="ri-arrow-down-line ri-lg"></i>
             </a>
@@ -49,7 +58,12 @@ function App() {
         />
       </div>
       <div className="tentang mt-32 py-10" id="tentang">
-        <div className="w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
+        <div
+          className="w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-once="true"
+        >
           <img
             src={DataImage.HeroImage}
             alt="Image"
@@ -84,21 +98,36 @@ function App() {
         </div>
 
         <div className="tools mt-32 ">
-          <h1 className="text-4xl/snug font-bold mb-4" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true"> tools</h1>
-          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true" >
-            tools yg aku pakai
+          <h1
+            className="text-4xl/snug font-bold mb-4 text-center"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-once="true"
+          >
+            Tools
+          </h1>
+          <p
+            className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="300"
+            data-aos-once="true"
+          >
+            Tools yang aku pakai
           </p>
           <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
             {listTools.map((tool) => (
               <div
-                className="flex items-center  gap-2 p-3 border border-zinc-800 rounded-md hover:bg-zinc-200 group "
+                className="flex items-center  gap-2 p-3 border border-[#A39383] rounded-md hover:bg-[#F1EAD8] group "
                 key={tool.id}
-                data-aos="fade-up" data-aos-duration="1000" data-aos-delay={tool.dad}
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay={tool.dad}
               >
                 <img
                   src={tool.gambar}
                   alt="tools image"
-                  className="w-14 bg-zinc-800 p-1 group-hover: bg-zinc-900"
+                  className="w-14 bg-transparent p-1 group-hover:bg-transparent rounded-md"
                 />
                 <div>
                   <h4 className="font-bold">{tool.nama}</h4>
@@ -111,13 +140,32 @@ function App() {
       </div>
 
       <div className="proyek mt-32 py-10" id="project">
-        <h1 className="text-center text-4xl font-bold mb-2" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">project</h1>
-        <p className="text-base/loose text-center opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
+        <h1
+          className="text-center text-4xl font-bold mb-2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-once="true"
+        >
+          project
+        </h1>
+        <p
+          className="text-base/loose text-center opacity-50"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="300"
+          data-aos-once="true"
+        >
           list project yang sudah aku buat
         </p>
         <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 ">
           {listProyek.map((proyek) => (
-            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md" data-aos="fade-up" data-aos-duration="1000" data-aos-delay={proyek.dad}>
+            <div
+              key={proyek.id}
+              className="p-4 bg-zinc-800 rounded-md"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay={proyek.dad}
+            >
               <img src={proyek.gambar} alt={proyek.nama} loading="lazy" />
               <div>
                 <h1 className="text2xl font-bold my-4">{proyek.nama}</h1>
@@ -188,9 +236,7 @@ function App() {
         </form>
       </div> */}
 
-      <div>
-
-      </div>
+      <div></div>
     </>
   );
 }
